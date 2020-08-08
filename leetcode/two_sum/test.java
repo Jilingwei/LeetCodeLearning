e43e89f1c739b2fbbb2246382861f727110cb8cf
姬凌伟 <jilingwei@jilingweideMacBook-Pro.local>
2020-08-08 19:21:20 +0800

    gogogo

---
+++ leetcode/two_sum/test.java
@@ -0,0 +1,11 @@
+package LeetCodeLearning.leetcode.two_sum;
+
+import java.util.HashMap;
+
+public class test {
+    public static void main(String[] args) {
+        HashMap map = new HashMap<>();
+        map.put(1, "a");
+        System.out.println(map.toString());
+    }
+}
